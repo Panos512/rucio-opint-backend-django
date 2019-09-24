2eca9e7cfe142db86affcb5d3e647913e1cb00dc
Panos Paparrigopoulos <panos.paparrigopoulos@cern.ch>
2019-09-24 14:16:32 +0200

    updates settings

--- rucio_opint_backend/apps/core/settings.py
+++ rucio_opint_backend/apps/core/settings.py
@@ -11,24 +11,24 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import sys
+# import sys
 
 MIGRATIONS_STORE_MODULE = 'migrations'
 MIGRATIONS_STORE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Load local configuration
-try:
-    from settings_local import config, DEBUG
-    DEBUG = str(os.environ.get('DJANGO_DEBUG', DEBUG)).lower() in ['true', '1']
-    vars().update(config)
-except ImportError as e:
-    msg = "File settings_local.py not found or incomplete.\nError: %s" % e
-    print('sys.path=', sys.path, 'PWD=', os.getcwd())
-    raise Exception(msg)
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# try:
+#     from settings_local import config, DEBUG
+#     DEBUG = str(os.environ.get('DJANGO_DEBUG', DEBUG)).lower() in ['true', '1']
+#     vars().update(config)
+# except ImportError as e:
+#     msg = "File settings_local.py not found or incomplete.\nError: %s" % e
+#     print('sys.path=', sys.path, 'PWD=', os.getcwd())
+#     raise Exception(msg)
+#
+# # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TMPDIR = os.path.join(BASE_DIR, 'tmp')
 API_KEY = os.environ.get('API_KEY')
 ALLOWED_HOSTS = ['*']
 
@@ -40,23 +40,23 @@ SECRET_KEY = 'i-cj+m#t+!rv6x4t1(2r^zt@@p4&x7pv)=of0xh-a6w&vs-e(1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-# DATABASES = {
-#     'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'NAME': 'rucio_opint',
-#             'USER': 'admin',
-#             'PASSWORD': os.environ.get('DB_PASS'),
-#             'USER_CREATE': 'cric',
-#             'PASSWORD_CREATE': os.environ.get('DB_PASS'),
-#             'HOST': 'dbod-rucio-opint.cern.ch',
-#             'PORT': '5501',
-#             'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
-#     }
-#     # 'default': {
-#     #     'ENGINE': 'django.db.backends.sqlite3',
-#     #     'NAME': 'rucio_opint',
-#     # }
-# }
+DATABASES = {
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.mysql',
+    #         'NAME': 'rucio_opint',
+    #         'USER': 'admin',
+    #         'PASSWORD': os.environ.get('DB_PASS'),
+    #         'USER_CREATE': 'cric',
+    #         'PASSWORD_CREATE': os.environ.get('DB_PASS'),
+    #         'HOST': 'dbod-rucio-opint.cern.ch',
+    #         'PORT': '5501',
+    #         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'rucio_opint',
+    }
+}
 
 
 # Application definition
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MIGRATIONS_STORE_PATH=os.path.join(BASE_DIR, 'migrations')
 MIGRATION_MODULES_LIST = ['core']
 MIGRATION_MODULES = {}
 MIGRATION_MODULES.update(dict([k, '%s.%s' % (MIGRATIONS_STORE_MODULE, k)] for k in MIGRATION_MODULES_LIST))
